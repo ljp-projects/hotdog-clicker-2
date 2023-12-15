ad154e73e4d77015a072ea49291cc8b34318dadc
ljp-projects <131211966+ljp-projects@users.noreply.github.com>
2023-12-15 12:18:26 +1100

    ho

--- pages/js/test.js
+++ pages/js/test.js
@@ -3,13 +3,15 @@ const stats = {
     money: 0,
     totalEarned: 0,
     totalClicks: 0,
-    updates: 0
+    updates: 0,
+    power: 1
 };
 var Elements;
 (function (Elements) {
     Elements[Elements["Money"] = 0] = "Money";
     Elements[Elements["TotalEarned"] = 1] = "TotalEarned";
     Elements[Elements["TotalClicks"] = 2] = "TotalClicks";
+    Elements[Elements["Power"] = 3] = "Power";
     Elements[Elements["Button"] = 0] = "Button";
 })(Elements || (Elements = {}));
 const getStat = (index) => {
@@ -19,15 +21,16 @@ const getGame = (index) => {
     return document.querySelectorAll("#game *")[index];
 };
 const update = () => {
-    const moneyElement = getStat(Elements.Money), totalEarnedElement = getStat(Elements.TotalEarned), totalClicksElement = getStat(Elements.TotalClicks), buttonElement = getGame(Elements.Button);
-    if (moneyElement && totalEarnedElement && totalClicksElement && buttonElement) {
+    const moneyElement = getStat(Elements.Money), totalEarnedElement = getStat(Elements.TotalEarned), totalClicksElement = getStat(Elements.TotalClicks), buttonElement = getGame(Elements.Button), powerElement = getStat(Elements.Power);
+    if (moneyElement && totalEarnedElement && totalClicksElement && buttonElement && powerElement) {
         moneyElement.textContent = stats.money.toFixed(2);
         totalEarnedElement.textContent = stats.totalEarned.toFixed(2);
-        totalClicksElement.textContent = stats.totalClicks.toFixed(2);
+        totalClicksElement.textContent = stats.totalClicks.toString();
+        powerElement.textContent = stats.power.toString();
         buttonElement.onclick = function () {
-            stats.money++;
+            stats.money += stats.power;
             stats.totalClicks++;
-            stats.totalEarned++;
+            stats.totalEarned += stats.power;
         };
         return true;
     }
