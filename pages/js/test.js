8e28dd194943da1a69b481447ac485fea3363701
ljp-projects <131211966+ljp-projects@users.noreply.github.com>
2023-12-15 11:58:56 +1100

    kinda wasteful but eh

--- pages/js/test.js
+++ pages/js/test.js
@@ -1,1 +1,49 @@
-console.log(document.querySelectorAll("#stats span")[0].textContent)
+"use strict";
+const stats = {
+    money: 0,
+    totalEarned: 0,
+    totalClicks: 0,
+    updates: 0
+};
+var Elements;
+(function (Elements) {
+    Elements[Elements["Money"] = 0] = "Money";
+    Elements[Elements["TotalEarned"] = 1] = "TotalEarned";
+    Elements[Elements["TotalClicks"] = 2] = "TotalClicks";
+    Elements[Elements["Button"] = 0] = "Button";
+})(Elements || (Elements = {}));
+const getStat = (index) => {
+    return document.querySelectorAll("#stats p span")[index];
+};
+const getGame = (index) => {
+    return document.querySelectorAll("#game *")[index];
+};
+const update = () => {
+    const moneyElement = getStat(Elements.Money), totalEarnedElement = getStat(Elements.TotalEarned), totalClicksElement = getStat(Elements.TotalClicks), buttonElement = getGame(Elements.Button);
+    if (moneyElement && totalEarnedElement && totalClicksElement && buttonElement) {
+        moneyElement.textContent = stats.money.toFixed(2);
+        totalEarnedElement.textContent = stats.totalEarned.toFixed(2);
+        totalClicksElement.textContent = stats.totalClicks.toFixed(2);
+        buttonElement.onclick = function () {
+            stats.money++;
+            stats.totalClicks++;
+            stats.totalEarned++;
+        };
+        return true;
+    }
+    return false;
+};
+function check(v, max) {
+    stats.updates++;
+    if (v) {
+        const r = update();
+        check(r, max);
+    }
+    else if (stats.updates < max) {
+        check(v, max);
+    }
+    else {
+        console.error(`Not executing script, required dom elements not found after ${max} tries.`);
+    }
+}
+check(update(), 10000);
